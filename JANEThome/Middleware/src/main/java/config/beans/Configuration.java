bbdb05818455e8e93fc084670f65e9aa1db1215a
Federico Lapenna <lapennafederico@gmail.com>
2021-09-28 10:40:27 +0200

    add auto get file .conf on src/main/resources/META-INF path

--- JANEThome/Middleware/src/main/java/config/beans/Configuration.java
+++ JANEThome/Middleware/src/main/java/config/beans/Configuration.java
@@ -8,29 +8,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.io.FilenameUtils;
 
 @Singleton
 public class Configuration implements ConfigurationInterface {
 
     private final HashMap<String,HashMap<String,String>> conf = new HashMap<>();
-    private static final String[] files = { "rabbit.conf", "rest.conf", "mail.conf", "tokens.conf" };
+    private static final String PATH_CONF = "src/main/resources/META-INF";
 
     public Configuration(){
-
         Gson gson = new Gson();
-        try{
 
-            for( String file : files ) {
-                String path = Objects.requireNonNull(this.getClass().getClassLoader().getResource("META-INF/" + file)).getPath();
+        try (Stream<Path> walk = Files.walk(Paths.get(PATH_CONF))) {
+            List<Path> result = walk.filter(Files::isRegularFile).filter(f->f.getFileName().toString().endsWith(".conf"))
+                    .collect(Collectors.toList());
+            result.forEach(System.out::println);
+
+            for( Path path : result ) {
+                Path rootPath = path.toAbsolutePath();
                 if (path != null)
-                    conf.put(file.replace(".conf", ""), gson.fromJson(new String(Files.readAllBytes(Paths.get(path))), new TypeToken<HashMap<String, String>>() {
+                    conf.put(FilenameUtils.removeExtension(path.getFileName().toString()),
+                            gson.fromJson(new String(Files.readAllBytes(rootPath)), new TypeToken<HashMap<String, String>>() {
                     }.getType()));
             }
-
-        }catch( IOException e ){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
