ecf2e6287aa209748cb25d0e4a4f5cebc6542d17
Federico Lapenna <lapennafederico@gmail.com>
2021-09-28 11:13:16 +0200

    clena IGenericDao

--- JANEThome/Middleware/src/main/java/db/interfaces/IGenericDao.java
+++ JANEThome/Middleware/src/main/java/db/interfaces/IGenericDao.java
@@ -1,10 +1,7 @@
 package db.interfaces;
 
-import java.util.Collection;
-
 public interface IGenericDao<T> {
-    //String DB_HOST = "mongodb+srv://Miucio:qyDfo1-pohdek-fygboz@smarthomelocations.uwuk3.mongodb.net/IoTServiceDB?retryWrites=true&w=majority";
-    String DB_HOST = "mongodb://127.0.0.1:27017/start?compressors=disabled&gssapiServiceName=mongodb";
+    String DB_HOST = "localhost";
     int DB_PORT = 27017;
     String DB_NAME = "IoTServiceDB";
 }
