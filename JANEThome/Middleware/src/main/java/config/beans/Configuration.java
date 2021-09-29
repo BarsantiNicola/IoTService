6304a9b393a65704df0c06b06282521b28df2630
Federico Lapenna <lapennafederico@gmail.com>
2021-09-29 17:33:23 +0200

    restore old Configuration.java

--- JANEThome/Middleware/src/main/java/config/beans/Configuration.java
+++ JANEThome/Middleware/src/main/java/config/beans/Configuration.java
@@ -3,37 +3,31 @@ package config.beans;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import config.interfaces.ConfigurationInterface;
+
 import javax.ejb.Singleton;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.apache.commons.io.FilenameUtils;
 
 @Singleton
 public class Configuration implements ConfigurationInterface {
 
-    private final HashMap<String,HashMap<String,String>> conf = new HashMap<>();
-    private static final String PATH_CONF = "src/main/resources/META-INF";
+    private final HashMap<String, HashMap<String, String>> conf = new HashMap<>();
+    private static final String[] files = {"db.conf","rabbit.conf", "rest.conf", "mail.conf", "tokens.conf"};
 
-    public Configuration(){
+    public Configuration() {
+
         Gson gson = new Gson();
+        try {
 
-        try (Stream<Path> walk = Files.walk(Paths.get(PATH_CONF))) {
-            List<Path> result = walk.filter(Files::isRegularFile).filter(f->f.getFileName().toString().endsWith(".conf"))
-                    .collect(Collectors.toList());
-            result.forEach(System.out::println);
-
-            for( Path path : result ) {
-                Path rootPath = path.toAbsolutePath();
+            for (String file : files) {
+                String path = Objects.requireNonNull(this.getClass().getClassLoader().getResource("META-INF/" + file)).getPath();
                 if (path != null)
-                    conf.put(FilenameUtils.removeExtension(path.getFileName().toString()),
-                            gson.fromJson(new String(Files.readAllBytes(rootPath)), new TypeToken<HashMap<String, String>>() {
+                    conf.put(file.replace(".conf", ""), gson.fromJson(new String(Files.readAllBytes(Paths.get(path))), new TypeToken<HashMap<String, String>>() {
                     }.getType()));
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,13 +35,13 @@ public class Configuration implements ConfigurationInterface {
 
     @Override
     public String getParameter(String conf_file, String parameter) {
-        if( this.conf.containsKey(conf_file))
+        if (this.conf.containsKey(conf_file))
             return this.conf.get(conf_file).get(parameter);
         return null;
     }
 
-    public HashMap<String,String> getConfiguration(String conf_file){
-        if( this.conf.containsKey(conf_file))
+    public HashMap<String, String> getConfiguration(String conf_file) {
+        if (this.conf.containsKey(conf_file))
             return this.conf.get(conf_file);
         return null;
     }
