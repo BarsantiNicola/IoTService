93e267f273615c0f258fca89dc5b04014d09c89c
Federico Lapenna <lapennafederico@gmail.com>
2021-10-23 19:14:49 +0200

    fix statistics problem

--- JANEThome/Middleware/src/main/java/statistics/Statistics.java
+++ JANEThome/Middleware/src/main/java/statistics/Statistics.java
@@ -4,34 +4,42 @@ import java.io.Serializable;
 import java.util.*;
 
 @SuppressWarnings({"unused", "deprecation"})
-public class Statistics implements Serializable{
+public class Statistics implements Serializable {
 
     private final List<Statistic> statistics;
 
-    public Statistics(){ statistics = new ArrayList<>(); }
+    public Statistics() {
+        statistics = new ArrayList<>();
+    }
 
-    public Statistics(List<Statistic> statistics ){
+    public Statistics(List<Statistic> statistics) {
         this.statistics = statistics;
     }
 
     //  function to add iteratively statistics. It is not important the order in which statistic instances
     //  will be added to the class, they will always be showed ordered by their date parameter
-    public void addStatistic(Statistic statistic){
-        for( Statistic s: statistics )
-            if( s.getX().after(statistic.getX())) {
-                statistics.add(statistics.indexOf(s), statistic);
-                break;
-            }
+    public void addStatistic(Statistic statistic) {
+        if (statistics.isEmpty()) {
+            statistics.add(statistic);
+        } else {
+            for (Statistic s : statistics)
+                if (s.getX().after(statistic.getX())) {
+                    statistics.add(statistics.indexOf(s), statistic);
+                    break;
+                }
+        }
     }
 
     //  gives back all the stored statistic instances
-    public List<Statistic> getStatistics(){ return statistics; }
+    public List<Statistic> getStatistics() {
+        return statistics;
+    }
 
     //  TODO to be removed only for testing purpouse
-    public static List<Statistic> buildTestEnvironment(){
-        return new Statistics( Arrays.asList(
-                new Statistic( new Date(2012, Calendar.JULY, 15), "0" ),
-                new Statistic( new Date(2012, Calendar.JULY, 18),"2000000"))).getStatistics();
+    public static List<Statistic> buildTestEnvironment() {
+        return new Statistics(Arrays.asList(
+                new Statistic(new Date(2012, Calendar.JULY, 15), "0"),
+                new Statistic(new Date(2012, Calendar.JULY, 18), "2000000"))).getStatistics();
 //                new Statistic( new Date(2012, Calendar.JULY, 23),6000000),
 //                new Statistic( new Date(2012, Calendar.AUGUST, 1),10000000),
 //                new Statistic( new Date(2012, Calendar.AUGUST, 11),21000000),
